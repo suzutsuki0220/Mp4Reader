409899535e88fe063cce5a6fcaf06c86a2ba60f9
suzutsuki0220 <suzutsuki0220@users.noreply.github.com>
2019-02-26 00:42:24 +0900

    feat: add font-awesome from webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const MODE = 'development';  // 'production' or 'development'
+
+// development に設定するとソースマップ有効でJSファイルが出力される
+const enabledSourceMap = (MODE === 'development');
+
+var path = require('path'),
+ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    mode: MODE,
+    entry: [
+        './node_modules/@fortawesome/fontawesome-free/css/all.css',
+        './_sass/main.scss'
+    ],
+    output: {
+        path: path.join(__dirname, 'app/'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(s)?css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true,
+                            sourceMap: enabledSourceMap,
+
+                            // 0 => no loaders (default);
+                            // 1 => postcss-loader;
+                            // 2 => postcss-loader, sass-loader
+                            importLoaders: 2
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: enabledSourceMap,
+                        }
+                    }
+                ],
+            },
+            {
+                // 画像など
+                test: /\.(gif|png|jpg)$/,
+                loader: 'url-loader'  // Base64化
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader"
+            }
+        ],
+    },
+    externals: [
+        'xmlhttprequest'
+    ],
+};
