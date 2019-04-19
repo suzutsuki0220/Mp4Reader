ba90d69e761ff91d81b2fb501c78658b91dcaa49
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-04-19 19:51:38 +0900

    refactor: reallocate creation and modification time view

--- app/scripts/mp4_atom_view.js
+++ app/scripts/mp4_atom_view.js
@@ -9,12 +9,36 @@ function makeTable(kv_array) {
     return "<table class='table is-bordered is-striped'>" + data + "</table>";
 }
 
+function bigIntString(payload, offset) {
+    return bigInt.getBigint(payload.readUIntBE(offset+4, 4), payload.readUIntBE(offset, 4)).toString();
+}
+
 function getFullBox(payload) {
     return {
         version: payload.readUIntBE(0, 1),
         flag: payload.readUIntBE(1, 3),
         size: 4
     };
+}
+
+function getCreationAndModificationTime(version, payload, offset) {
+    if (version === 1) {
+        return {
+            data: [
+                {key: "Creation Time", value: bigIntString(payload, offset)},
+                {key: "Modification Time", value: bigIntString(payload, offset+8)}
+            ],
+            size: 16
+        };
+    } else { // if (version == 0)
+        return {
+            data: [
+                {key: "Creation Time", value: decode.dateTime(payload, offset)},
+                {key: "Modification Time", value: decode.dateTime(payload, offset+4)},
+            ],
+            size: 8
+        };
+    }
 }
 
 module.exports.parseFileTypeBox = function(payload) {
@@ -33,23 +57,16 @@ module.exports.parseMovieHeaderBox = function(payload) {
     var kv_array = [
         {key: "Version", value: fullbox.version}
     ];
-    if (fullbox.version == 1) {
-        kv_array = kv_array.concat([
-            {key: "Creation Time", value: bigInt.getBigint(payload.readUIntBE(offset+4, 4), payload.readUIntBE(offset+0, 4)).toString()},
-            {key: "Modification Time", value: bigInt.getBigint(payload.readUIntBE(offset+12, 4), payload.readUIntBE(offset+8, 4)).toString()},
-            {key: "Timescale", value: payload.readUIntBE(offset+16, 4)},
-            {key: "Duration", value: bigInt.getBigint(payload.readUIntBE(offset+24, 4), payload.readUIntBE(offset+20, 4)).toString()}
-        ]);
-        offset += 28
-    } else {  // if (version == 0)
-        kv_array = kv_array.concat([
-            {key: "Creation Time", value: decode.dateTime(payload, offset)},
-            {key: "Modification Time", value: decode.dateTime(payload, offset+4)},
-            {key: "Timescale", value: payload.readUIntBE(offset+8, 4)},
-            {key: "Duration", value: payload.readUIntBE(offset+12, 4)}
-        ]);
-        offset += 16
-    }
+    const timedata = getCreationAndModificationTime(fullbox.version, payload, offset);
+    kv_array = kv_array.concat(timedata.data);
+    offset += timedata.size;
+
+    kv_array = kv_array.concat([
+        {key: "Timescale", value: payload.readUIntBE(offset, 4)},
+        {key: "Duration", value: fullbox.version === 1 ? bigIntString(offset+4) : payload.readUIntBE(offset+4, 4)}
+    ]);
+    offset += fullbox.version === 1 ? 12 : 8;
+
     kv_array = kv_array.concat([
         {key: "Rate", value: payload.readUIntBE(offset, 4)},
         {key: "Volume", value: payload.readUIntBE(offset+4, 2)},
@@ -66,25 +83,17 @@ module.exports.parseTrackHeaderBox = function(payload) {
     var kv_array = [
         {key: "Version", value: fullbox.version}
     ];
-    if (fullbox.version == 1) {
-        kv_array = kv_array.concat([
-            {key: "Creation Time", value: bigInt.getBigint(payload.readUIntBE(offset+4, 4), payload.readUIntBE(offset+0, 4)).toString()},
-            {key: "Modification Time", value: bigInt.getBigint(payload.readUIntBE(offset+12, 4), payload.readUIntBE(offset+8, 4)).toString()},
-            {key: "Track ID", value: payload.readUIntBE(offset+16, 4)},
-            /* reserved: 4 */
-            {key: "Duration", value: bigInt.getBigint(payload.readUIntBE(offset+28, 4), payload.readUIntBE(offset+24, 4)).toString()}
-        ]);
-        offset += 32
-    } else {  // if (version == 0)
-        kv_array = kv_array.concat([
-            {key: "Creation Time", value: decode.dateTime(payload, offset)},
-            {key: "Modification Time", value: decode.dateTime(payload, offset+4)},
-            {key: "Track ID", value: payload.readUIntBE(offset+8, 4)},
-            /* reserved: 4 */
-            {key: "Duration", value: payload.readUIntBE(offset+16, 4)}
-        ]);
-        offset += 20
-    }
+    const timedata = getCreationAndModificationTime(fullbox.version, payload, offset);
+    kv_array = kv_array.concat(timedata.data);
+    offset += timedata.size;
+
+    kv_array = kv_array.concat([
+        {key: "Track ID", value: payload.readUIntBE(offset, 4)},
+        /* reserved: 4 */
+        {key: "Duration", value: fullbox.version === 1 ? bigIntString(payload, offset+8) : payload.readUIntBE(offset+8, 4)}
+    ]);
+    offset += fullbox.version === 1 ? 16 : 12;
+
     kv_array = kv_array.concat([
         {key: "Layer", value: payload.readUIntBE(offset+8, 2)},
         {key: "Alternate Group", value: payload.readUIntBE(offset+10, 2)},
@@ -103,23 +112,16 @@ module.exports.parseMediaHeaderBox = function(payload) {
     var kv_array = [
         {key: "Version", value: fullbox.version}
     ];
-    if (fullbox.version == 1) {
-        kv_array = kv_array.concat([
-            {key: "Creation Time", value: bigInt.getBigint(payload.readUIntBE(offset+4, 4), payload.readUIntBE(offset+0, 4)).toString()},
-            {key: "Modification Time", value: bigInt.getBigint(payload.readUIntBE(offset+12, 4), payload.readUIntBE(offset+8, 4)).toString()},
-            {key: "Time Scale", value: payload.readUIntBE(offset+16, 4)},
-            {key: "Duration", value: bigInt.getBigint(payload.readUIntBE(offset+24, 4), payload.readUIntBE(offset+20, 4)).toString()}
-        ]);
-        offset += 28
-    } else {  // if (version == 0)
-        kv_array = kv_array.concat([
-            {key: "Creation Time", value: decode.dateTime(payload, offset)},
-            {key: "Modification Time", value: decode.dateTime(payload, offset+4)},
-            {key: "Time Scale", value: payload.readUIntBE(offset+8, 4)},
-            {key: "Duration", value: payload.readUIntBE(offset+12, 4)}
-        ]);
-        offset += 16
-    }
+    const timedata = getCreationAndModificationTime(fullbox.version, payload, offset);
+    kv_array = kv_array.concat(timedata.data);
+    offset += timedata.size;
+
+    kv_array = kv_array.concat([
+        {key: "Timescale", value: payload.readUIntBE(offset, 4)},
+        {key: "Duration", value: fullbox.version === 1 ? bigIntString(payload, offset+4) : payload.readUIntBE(offset+4, 4)}
+    ]);
+    offset += fullbox.version === 1 ? 12 : 8;
+
     kv_array = kv_array.concat([
         {key: "Language", value: decode.languageString(payload, offset)},
         {key: "Quality", value: payload.readUIntBE(offset+2, 2)}
