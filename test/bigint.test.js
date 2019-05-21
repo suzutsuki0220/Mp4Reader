17bd307b20d2bca0f7519b4c6287a6e8abdb0cb4
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-05-21 10:40:16 +0000

    test: fix bigint test to pass

--- test/bigint.test.js
+++ test/bigint.test.js
@@ -1,17 +1,17 @@
 describe('bigint', () => {
   let bigint;
   beforeEach(() => {
-    bigint = require('../app/scripts/bigint.js');
-    b = require('big-integer');
+    bigint = require('../lib/bigint.js');
+    //b = require('big-integer');
   })
 
-  describe('getBigint()', () => {
-    test('getBigint', () => {
-      expect(bigint.getBigint(0, 1)).toEqual(b(1));
-      expect(bigint.getBigint(0, 4294967295)).toEqual(b(4294967295));
-      expect(bigint.getBigint(1, 0)).toEqual(b(4294967296));
-      expect(bigint.getBigint(0x1fffff, 0xffffffff)).toEqual(b(9007199254740991));
-      expect(bigint.getBigint(0x200000, 0)).toEqual(b(9007199254740992));
+  describe('getIntStringBE()', () => {
+    test('getIntStringBE', () => {
+      expect(bigint.getIntStringBE(Buffer.from([0,0,0,1,0,0,0,0]))).toEqual("1");
+      expect(bigint.getIntStringBE(Buffer.from([0xFF,0xFF,0xFF,0xFF,0,0,0,0]))).toEqual("4294967295");
+      expect(bigint.getIntStringBE(Buffer.from([0,0,0,0,0,0,0,1]))).toEqual("4294967296");
+      expect(bigint.getIntStringBE(Buffer.from([0xff,0xff,0xff,0xff,0,0x1f,0xff,0xff]))).toEqual("9007199254740991");
+      expect(bigint.getIntStringBE(Buffer.from([0,0,0,0,0,0x20,0,0]))).toEqual("9007199254740992");
     })
   })
 })
