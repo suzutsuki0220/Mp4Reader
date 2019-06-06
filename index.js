93a0c26a95edb9addbf395d2d311ca9116f188db
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-06-06 11:00:58 +0000

    fix: library require miss

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-module.exports.fileLoader = require('./fileLoader.js');
-module.exports.atomTable = require('./mp4/atom_table.js');
+module.exports.fileLoader = require('./lib/fileLoader.js');
+module.exports.atomTable = require('./lib/mp4/atom_table.js');
